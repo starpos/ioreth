95214892f31cb56b7fc5282a1689e80bb2c5ff5f
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2012-03-19 18:36:09 +0900

    modify plot_bps.py

--- scripts/plot_bps.py
+++ scripts/plot_bps.py
@@ -16,12 +16,18 @@ from performance_plot import *
 def doMain():
 
   if len(sys.argv) < 2:
-    print "Usage %s [throughput.plot]" % sys.argv[0]
+    print "Usage %s [throughput.plot] (template) ([yrange])" % sys.argv[0]
+    print r"template is like 'bps_%s_bs%s.png'"
+    print r"yrange is like '0:*'"
   throughputPlotPath = sys.argv[1]
   if len(sys.argv) >= 3:
     outputTemplate = sys.argv[2]
   else:
     outputTemplate = 'bps_%s_bs%s.png'
+  if len(sys.argv) >= 4:
+    yRange = sys.argv[3]
+  else:
+    yRange = '0:*'
 
   patternMap = {'rnd':'random', 'seq':'sequential'}
   pairs = [(x, y) for x in ['rnd', 'seq'] for y in ['512', '4k', '32k', '256k']]
@@ -36,7 +42,7 @@ def doMain():
   ylabel = 'Throughput [MB/sec]'
 
   plotPerformanceData(c, titleTemplate, outputTemplate, targetColumn, ylabel, patternMap, pairs,
-                      scale=Decimal('1')/Decimal('1000000'), xRange='*:*', yRange='0:5000')
+                      scale=Decimal('1')/Decimal('1000000'), xRange='*:*', yRange=yRange)
   f.close()
 
 if __name__ == "__main__":
